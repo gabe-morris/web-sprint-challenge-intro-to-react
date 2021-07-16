7a7228e01c2b3e6e0a4c604189dbcc37339129ec
gabe-morris <gabe.e.morris@gmail.com>
2021-07-16 14:12:49 -0500

    added names and button to UI

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
-
+import axios from 'axios'
+import styled from 'styled-components'
+import Character from './components/Character'
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
-  //RANDOM COMMENT FOR CODEGRADE COMMIT
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+    const [char, setChar] = useState([])
+    const [currentChar,setCurrentChar] = useState(null)
+    const openDetails = char => {
+      setCurrentChar(char)
+    }
+    const closeDetails = () => {
+      setCurrentChar(null)
+    }
+  useEffect(() => {
+    axios.get(`https://swapi.dev/api/people`)
+    .then (res => setChar(res.data))
+  },[])
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <Character />
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,25 @@
-// Write your Character component here
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+import styled, { ThemeProvider } from 'styled-components'
+
+const Character = () => {
+    const [details, setDetails] = useState([])
+    useEffect(()=>{
+        axios.get(`https://swapi.dev/api/people`)
+        .then(res => setDetails(res.data))
+        .catch(err => console.log(err))
+    },[])
+    return(
+        details.map(char => {
+            return <div>{char.name}
+            <button>{char.birth_year}</button>
+            </div>
+        })
+    )
+    }
+
+
+
+
+
+export default Character
